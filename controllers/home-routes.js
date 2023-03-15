7cbe701532359105e799e4e014ee2106c00b7529
jacko2611 <jacksonkent70@gmail.com>
2023-03-16 10:43:20 +1100

    altered home-routes.js to accomodate comment and post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
+const { Post, Comment } = require('../models');
 
 // Homepage route
 router.get('/', async (req, res) => {
